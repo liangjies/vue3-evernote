dfe756c26233af79afc55e1906a5a0e03ab3233a
liangjies <1139629972@qq.com>
2022-03-09 19:01:52 +0800

    commit

--- src/components/SiderBar.vue
+++ src/components/SiderBar.vue
@@ -25,9 +25,17 @@
               <notebook />
             </el-icon>
           </router-link>
-  <div style="margin: 15px">
-    <el-button @click="showClick">show</el-button>
-  </div>
+          <div class="pop-over">
+            <a @click="toggleOpen" class="pop-button" href="javascript: void(0);">
+              按钮
+            </a>
+            <ul v-clickoutside="close" v-show="open" class="pop-list">
+              <li>选项1</li>
+              <li>选项2</li>
+              <li>选项3</li>
+              <li>选项4</li>
+            </ul>
+          </div>
           <div class="userMenu">
             <img src="https://s1.ax1x.com/2022/03/07/b6amRI.png" alt="" class="userMenus-image">
           </div>
@@ -38,56 +46,23 @@
         <i class="el-icon-switch-button"></i>
       </div>
     </div>
-    <div class="user-pop">
-      <div class="user-pop-img">
-        <img src="https://s1.ax1x.com/2022/03/07/b6amRI.png" alt="" class="user-image">
-        <div class="user-name">{{ userName }}</div>
-        <div class="user-type">印象笔记免费帐户</div>
-      </div>
-      <div class="user-pop-func">
-        <div class="user-pop-ourside user-setting">
-          <div class="user-pop-box">
-            <el-icon class="user-pop-icon">
-              <setting />
-            </el-icon>
-            <span class="user-pop-text">设置</span>
-          </div>
-        </div>
-        <div class="user-pop-ourside user-question">
-          <div class="user-pop-box">
-            <el-icon class="user-pop-icon">
-              <question-filled />
-            </el-icon>
-            <span class="user-pop-text">使用指南</span>
-          </div>
-        </div>
-        <div class="user-pop-ourside user-logout">
-          <div class="user-pop-box">
-            <el-icon class="user-pop-icon">
-              <circle-close />
-            </el-icon>
-            <span class="user-pop-text">退出登录</span>
-          </div>
-        </div>
-      </div>
-    </div>
+    <user-pop></user-pop>
   </div>
 </template>
 
 <script>
-import { Plus, Document, Notebook, Search, CircleClose, Setting, QuestionFilled } from "@element-plus/icons-vue";
+import { Plus, Document, Notebook, Search } from "@element-plus/icons-vue";
+import UserPop from './UserPop.vue'
 // import AvatarBar from './AvatarBar.vue'
 // import SearchBar from './SearchBar.vue'
 export default {
   name: "SiderBar",
   components: {
+    UserPop,
     Plus,
     Document,
     Notebook,
-    Search,
-    CircleClose,
-    Setting,
-    QuestionFilled
+    Search
   },
   created() {
     // if (this.$store.getters['isLogin'] === false) {
@@ -107,6 +82,7 @@ export default {
     return {
       userName: "梁杰s",
       isCollapse: false,
+      open: false,
     };
   },
   computed: {
@@ -138,13 +114,33 @@ export default {
     */
   },
   methods: {
-    showClick() {
-      dropdown1.value.handleOpen()
+    toggleOpen: function () {
+      console.log("toggleOpen")
+      this.open = !this.open;
     },
-    test(){
+    close: function (e) {
+      console.log("close")
+      if (this.$el.contains(e.target)) return;
+      this.open = false;
+    },
+    test() {
       console.log("run")
     }
   },
+  directives: {
+    clickoutside: {
+      bind: function (el, binding, vnode) {
+        const documentHandler = function (e) {
+          if (!vnode.context || el.contains(e.target)) return;
+          binding.value(e);
+        };
+
+        setTimeout(() => {
+          document.addEventListener('click', documentHandler);
+        }, 0);
+      }
+    }
+  }
 };
 </script>
 
@@ -246,77 +242,6 @@ export default {
       display: flex;
       i {
         margin: auto;
-      }
-    }
-  }
-  .user-pop {
-    position: fixed;
-    top: 438px;
-    left: 63px;
-    background: #fff;
-    border: 1px solid #ececec;
-    border-radius: 4px;
-    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
-    box-sizing: border-box;
-    transition: opacity 500ms ease-in-out;
-    width: 354px;
-    overflow: hidden;
-    z-index: 9001;
-    .user-pop-img {
-      text-align: center;
-      .user-image {
-        border: 1px solid #d1d1d1;
-        border-radius: 50%;
-        height: 62px;
-        margin-top: 20px;
-        vertical-align: top;
-        width: 62px;
-      }
-      .user-name{
-        color: #4a4a4a;
-        margin-top: 11px
-      }
-      .user-type{
-        display: inline-block;
-        vertical-align: top;
-        color: #ababab;
-        max-width: 100%;
-        overflow-wrap: break-word;
-        word-wrap: break-word;
-        font-size: 11px;
-        font-weight: 500;
-      }
-    }
-    .user-pop-func{
-      margin: 0 12px 12px;
-      padding-top: 15px;
-      border-top: 1px solid #e1e1e1;
-      top: -1px;
-      position: relative;
-      .user-pop-ourside{
-        cursor: pointer;
-        height: 32px;
-      }
-      .user-pop-box{
-        padding-top: 3px;
-        .user-pop-icon {
-          color: #4a4a4a;
-          margin-left: 7px;
-          vertical-align: middle;
-        }
-        .user-pop-text {
-          padding: 9px 15px 10px 5px;
-          font-size: 13px;
-          font-weight: 500;
-          color: #4a4a4a;
-          max-width: 298px;
-        }
-      }
-      .user-pop-ourside:hover{
-        background-color: #2dbe60;
-        .user-pop-text,.user-pop-icon {
-           color: #fff;
-        }
       }
     }
   }
