f9ed850cc192efffb3858e63146fddee866fccc9
liangjies <1139629972@qq.com>
2023-02-04 23:00:13 +0800

    add: 引入markdown编辑器

---
+++ src/views/note/NoteMd.vue
@@ -0,0 +1,23 @@
+<template>
+  <md-editor v-model="text" />
+</template>
+
+<script>
+export default {
+  name: "NoteMd",
+};
+</script>
+<script setup>
+import { ref } from "vue";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+const text = ref("# Hello Editor");
+</script>
+<style lang="less" scoped>
+#editor #md-editor-v3.md-editor {
+  height: 85vh;
+}
+// .md-editor-footer {
+//   height: 40px;
+// }
+</style>
